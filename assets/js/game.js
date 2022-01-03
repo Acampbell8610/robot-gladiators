73564eb2f6aefb2c943ad9cd84ce2e7cf82619a5
acamp <acampbell8610@gmail.com>
2022-01-02 21:01:57 -0600

    adding console log

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,14 @@
-window.alert("This is an alert! Javascript is running!")
+// this creates a function named "fight"
+
+function fight () {
+    window.alert("The fight has begun!");
+}
+var playername = window.prompt("What is your robots's name?");
+
+console.log("This is a string, good for leaving yourself a message");
+
+console.log(10+10);
+
+console.log("Our robots's name is " + playername);
+
+//fight();
