f23f505a42985a99276f2c15e9a967f78e6ea886
acamp <acampbell8610@gmail.com>
2022-01-07 22:10:33 -0600

    bug fix flayer name

--- assets/js/game.js
+++ assets/js/game.js
@@ -75,6 +75,7 @@ for (var i = 0; i < enemyInfo.length; i++) {
 if (playerInfo.health > 0) {
   // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
   window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
+ 
 
   // pick new enemy to fight based on the index of the enemy.names array
   var pickedEnemyObj = enemyInfo[i];
@@ -153,9 +154,19 @@ switch (shopOptionPrompt) {
         break;
 }
 };
+// function set name
+var getPlayerName = function(){
+    var name = "";
 
+ while ( name === "" || name === null){
+     name = prompt("What is your robot's name?");
+ }
+
+    console.log("Your robot's name is" + name);
+    return name;
+}
 var playerInfo = {
-name: window.prompt("Wha is your Robot's name?"),
+name: getPlayerName(),
 health:100 ,
 attack:10 ,
 money : 10,
