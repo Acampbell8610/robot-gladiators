e40e9fa3d57dbbc9fe03ca1ff2a70b1cc044670b
acamp <acampbell8610@gmail.com>
2022-01-08 00:05:18 -0600

    highscore

--- assets/js/game.js
+++ assets/js/game.js
@@ -144,6 +144,24 @@ endGame();
 // endgame function
 var endGame = function() {
 window.alert("The game is now ended. Let's see how you did!");
+
+// check local storage for high score, if not there use 0
+var highScore= localStorage.getItem("highscore");
+if (highScore ===0){
+    highScore = 0;
+}
+
+//if player has more money than the high score, player has new high score
+if (playerInfo.money > highScore) {
+    localStorage.setItem("highscore" ,playerInfo.money);
+    localStorage.setItem("name", playerInfo.name);
+
+    alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+}
+else{
+    alert(playerInfo.name + " did not beat the high score of " + highScore + ".");
+}
+
 if (playerInfo.health > 0) {
     window.alert("Great job, you've survived the game. You now have a score of "+ playerInfo.money +". ");
 
